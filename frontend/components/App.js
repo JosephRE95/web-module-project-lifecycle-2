b52c236b995dd92b0ef9aada0391057b8f92916c
Joseph Ellis <josephellis95@gmail.com>
2023-12-30 12:58:13 -0600

    created error message

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,46 +1,48 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
+import { response } from "msw";
 
-const URL = 'http://localhost:9000/api/todos'
+const URL = "http://localhost:9000/api/todos";
 
 export default class App extends React.Component {
   state = {
     todos: [],
-  }
+  };
 
-  fetchAllTodos =  () => {
-    axios.get(URL)
-    .then( res => {
-      this.setState({ ...this.state, todos: res.data.data})
-    })
-    .catch(
-      res => {
-        debugger
+  fetchAllTodos = () => {
+    axios
+      .get(URL)
+      .then((res) => {
+        this.setState({ ...this.state, todos: res.data.data });
       })
-  }
+      .catch((err) => {
+        this.setState({ ...this.state, error: err.response.data.message });
+      });
+  };
 
-  componentDidMount(){
-    this.fetchAllTodos()
+  componentDidMount() {
+    this.fetchAllTodos();
   }
   render() {
-    return <div>
-    <div id='error'> Error: No error here </div>
-    <div className='todos'>
-      <h2>Todos:</h2>
-      {
-        this.state.todos.map(td => {
-          return <div key={td.id}>{td.name}</div>
-        })
-      }
-    </div>
+    return (
+      <div>
+        <div id="error">
+          {this.state.error && <span>Error: {this.state.error}</span>}
+        </div>
 
-    <form>
+        <div className="todos">
+          <h2>Todos:</h2>
+          {this.state.todos.map((td) => {
+            return <div key={td.id}>{td.name}</div>;
+          })}
+        </div>
 
-      <input></input>
-      <input></input>
-      <button>Clear Completed</button>
-    </form>
-
-    </div>
+        <form>
+          <input></input>
+          <input></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    );
   }
 }
