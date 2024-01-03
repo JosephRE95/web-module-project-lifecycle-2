125310cc2d2182c0ecda144d78422cccf7ba66bd
Joseph Ellis <josephellis95@gmail.com>
2024-01-03 11:57:32 -0600

    added comments to code

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -10,43 +10,53 @@ export default class App extends React.Component {
     toDoInput: "",
   };
 
+  // Update the state with the input value when there's a change in the input field
   onToDoInputChange = (evt) => {
     const { value } = evt.target;
-    this.setState({ toDoInput: value });
+    this.setState({ ...this.state, toDoInput: value });
   };
 
+  // Reset the toDoInput state to an empty string
   resetForm = () => {
     this.setState({ toDoInput: "" });
   };
 
+  // Update the state with the error message when there's an Axios error
   axiosError = (err) => {
     this.setState({ ...this.state, error: err.response.data.message });
   };
 
+  // Add a new todo item by making a POST request to the API
   postNewTodo = () => {
-    axios
-      .post(URL, { name: this.state.toDoInput })
-      .then((res) => {
-        this.fetchAllTodos();
-        this.resetForm();
-      })
-      .catch(this.axiosError);
-  };
+    axios.post(URL, { name: this.state.toDoInput })
+    .then( res => {
+      // Update todos state by concatenating the new todo item
+      this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data)})
+      this.resetForm(); // Reset the form after adding a todo
+    })
+    .catch(
+      this.axiosError // Catch any errors and update the error state
+    )
+  }
 
+  // Handle form submission
   onTodoFormSubmit = (evt) => {
     evt.preventDefault();
-    this.postNewTodo();
+    this.postNewTodo(); // Call method to add a new todo
   };
 
+  // Fetch all todos from the API
   fetchAllTodos = () => {
     axios
       .get(URL)
       .then((res) => {
+        // Update todos state with the fetched data and reset error state
         this.setState({ ...this.state, todos: res.data.data, error: "" });
       })
-      .catch(this.axiosError);
+      .catch(this.axiosError); // Catch any errors and update the error state
   };
 
+  // Fetch all todos when the component mounts
   componentDidMount() {
     this.fetchAllTodos();
   }
